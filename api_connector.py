5aeda3548a785c93d8d2b25d24f93a2d8dd9867c
Meziu <meziu210@icloud.com>
2020-07-14 23:11:38 +0200

    Server Status command implemented

---
+++ api_connector.py
@@ -0,0 +1,52 @@
+import requests as r
+from tabulate import tabulate
+
+api_url = "https://srb2circuit.eu/highscores/api/"
+
+def tics_to_string(time):
+    minutes = round(time/(60*35))
+    seconds = round(time/35%60)
+    centiseconds = round((time%35) * (100/35))
+    return f"{minutes}:{seconds}.{centiseconds}"
+
+def markup(txt):
+    return "```"+txt+"```"
+
+def get_leaderboard():
+    # request the json for the leaderboard
+    leaderboard = r.get(api_url+"leaderboard", verify=False).json()
+    
+    # this way the leaderboard stops at the very last score and not in the middle of one
+    # the maximum sting length is 2000 max chars(of the discord message) - 6 chars(for the markup signs) - the modulus of the current result over the max chars for each line
+    str_length = 2000 - 6 - ((2000 - 6) % 31)
+    
+    # create the table and return it
+    return markup(tabulate(leaderboard.items(), headers=["Player", "Points"])[:str_length])
+
+def get_status():
+    # request the json for the server status
+    status = r.get(api_url+"server_info", verify=False).json()
+    
+    time_str = tics_to_string(status['leveltime'])
+    
+    players_list = []
+    
+    for player in status['players']:
+        temp = []
+        
+        temp.append(player['num'])
+        temp.append(player['name'])
+        temp.append(player['skin'])
+        
+        players_list.append(temp)
+
+    players_str = tabulate(players_list, headers=["ID", "Username", "Skin"])
+    
+    res = (f"Server: {status['servername']}\n\n" 
+           f"Number of Players: {status['number_of_players']}/{status['max_players']}\n"
+           f"Map: {status['map']['name']} (id = {status['map']['id']})\n"
+           f"Level Time: {time_str}\n\n"
+           f"Players: \n{players_str}"
+    )
+
+    return markup(res)
--- app.py
+++ app.py
@@ -1,45 +1,27 @@
-import discord
-from commands import cmd_list
+from discord.ext import commands
+import api_connector as con
 from credentials import TOKEN
 
 # initialize a Client instance
-bot_client = discord.Client()
+bot = commands.Bot(command_prefix="race!")
 
 # when the bot starts running
-@bot_client.event
+@bot.event
 async def on_ready():
     # print the bot's username
-    print('Logged in as '+ bot_client.user.name)
+    print('Logged in as '+ bot.user.name)
 
-# when a message is received
-@bot_client.event
-async def on_message(message):
-    # if the message is from the bot or the channel isn't "bot_commands"
-    if message.author == bot_client.user or message.channel.name != "bot_commands":
-        # don't do anything
-        return
+# race!leaderboard command received
+@bot.command()
+async def leaderboard(ctx):
+    await ctx.send(con.get_leaderboard())
     
-    # if the message starts with "race!"
-    if message.content.startswith('race!'):
-        # receive the message and split it to get the command and arguments
-        received_cmd = message.content.split('race!')[1].split(" ")
-        cmd = received_cmd[0]
-        args = received_cmd[1::1]
-        
-        msg = None
-        
-        # if the given command is a valid command
-        for t in cmd_list.keys():
-            if cmd == t:
-                # set the message to the result of its function
-                msg = cmd_list[t]()
-
-        # if a message has been set
-        if msg:
-            # send the message
-            await message.channel.send("```"+msg+"```")
-                
+# race!status command received
+@bot.command()
+async def status(ctx):
+    await ctx.send(con.get_status())
+ 
 # run
 if __name__== "__main__":
     # connect the client to the bot
-    bot_client.run(TOKEN)
+    bot.run(TOKEN)
