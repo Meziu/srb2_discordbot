eeb8f297a02efbf75725dcab419c0b40609a1e8c
Meziu <meziu210@icloud.com>
2020-07-15 13:56:12 +0200

    Help command created

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
+from discord import Embed, Colour
 from discord.ext import commands
 import api_connector as con
 from credentials import TOKEN
 
 # initialize a Client instance
-bot = commands.Bot(command_prefix="race!")
+bot = commands.Bot(command_prefix="!")
+bot.remove_command('help')
 
 # when the bot starts running
 @bot.event
@@ -21,9 +23,29 @@ async def leaderboard(ctx):
 async def status(ctx):
     await ctx.send(con.get_status())
 
+# race!search command received
 @bot.command()
 async def search(ctx, map=None, skin=None, player=None):
     await ctx.send(con.get_search_result(map, skin, player))
+
+# race!help command received
+@bot.command()
+async def help(ctx):
+    embed = Embed(colour=Colour.orange())
+    
+    embed.set_author(name="Help")
+    
+    embed.add_field(name="race!status", value="Returns the server status", inline=False)
+    embed.add_field(name="race!leaderboard", value="Returns the player leaderboard", inline=False)
+    embed.add_field(name="race!search", value=('Usage: race!search "<map name>" "[skin name]" "[username]"\n''All parameters can be submitted with no "" if they '"don't require spaces"), inline=False)
+    
+    member = ctx.message.author
+    
+    if member:
+        channel = member.dm_channel
+        if not channel:
+            channel = await member.create_dm()
+        await channel.send(embed=embed)
 
 # run
 if __name__== "__main__":
